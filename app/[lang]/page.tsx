56ddbf39a0b7a31cb1215797f54190e3515bbc2a
Fernando Dingler <fdingler@amazon.com>
2023-08-14 12:28:57 -0700

    Removed hello world

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,9 +2,6 @@ import DropDownImage from './components/dropdownimage'
 
 export default function Home() {
   return (
-    <>
-    <h1>Hello world</h1>
     <DropDownImage className={''} />
-    </>
   )
 }
