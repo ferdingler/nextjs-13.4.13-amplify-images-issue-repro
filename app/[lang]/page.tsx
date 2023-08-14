1b84af30861afe39ffd7c139ff4f12fbc1d45f91
Fernando Dingler <fdingler@amazon.com>
2023-08-14 12:17:42 -0700

    hello world

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,6 +2,9 @@ import DropDownImage from './components/dropdownimage'
 
 export default function Home() {
   return (
+    <>
+    <h1>Hello world</h1>
     <DropDownImage className={''} />
+    </>
   )
 }
